54d129dc49cd91875a0d3c680af982600cfd88f0
KilowattSynthesis <180148956+KilowattSynthesis@users.noreply.github.com>
2024-12-22 17:11:31 -0700

    Show dots in vertical motor layers demo

--- cad/vertical_motor_layers.py
+++ cad/vertical_motor_layers.py
@@ -39,16 +39,21 @@ def evenly_space_with_center(
 class HousingSpec:
     """Specification for braille cell in general."""
 
-    dot_pitch_x: float = 3
-    dot_pitch_y: float = 3
+    motor_pitch_x: float = 3
+    motor_pitch_y: float = 3
+    dot_pitch_x: float = 2.5
+    dot_pitch_y: float = 2.5
     inter_cell_pitch_x: float = 6
     inter_cell_pitch_y: float = 10
 
     motor_body_od: float = 4.0
     motor_body_length: float = 8.0
 
+    gap_between_motor_layers: float = 1
+    gap_above_top_motor: float = 3
+
     cell_count_x: int = 3
-    cell_count_y: int = 2
+    cell_count_y: int = 1
 
     # TODO(KilowattSynthesis): Set these as properties
     total_x: float = 50
@@ -57,8 +62,15 @@ class HousingSpec:
 
     def __post_init__(self) -> None:
         """Post initialization checks."""
-        hypot_len = (self.dot_pitch_x**2 + self.dot_pitch_y**2) ** 0.5
+        hypot_len = (self.motor_pitch_x**2 + self.motor_pitch_y**2) ** 0.5
         logger.info(f"Hypotenuse length: {hypot_len:.2f} mm")
+
+        total_thickness = (
+            self.motor_body_length * 2
+            + self.gap_between_motor_layers
+            + self.gap_above_top_motor
+        )
+        logger.info(f"Total thickness: {total_thickness:.2f} mm")
 
     def deep_copy(self) -> "HousingSpec":
         """Copy the current spec."""
@@ -80,12 +92,14 @@ def make_motor_placement_demo(spec: HousingSpec) -> bd.Part:
                 count=spec.cell_count_y,
                 spacing=spec.inter_cell_pitch_y,
             ),
-            evenly_space_with_center(count=2, spacing=spec.dot_pitch_x),
-            evenly_space_with_center(count=3, spacing=spec.dot_pitch_y),
+            evenly_space_with_center(count=2, spacing=spec.motor_pitch_x),
+            evenly_space_with_center(count=3, spacing=spec.motor_pitch_y),
         ),
     ):
         dot_x = cell_x + dot_offset_x
         dot_y = cell_y + dot_offset_y
+
+        layer_num = dot_num % 2  # 0 (bottom) or 1 (top)
 
         # Create the motor hole.
         p += bd.Cylinder(
@@ -97,21 +111,61 @@ def make_motor_placement_demo(spec: HousingSpec) -> bd.Part:
             (
                 dot_x,
                 dot_y,
-                (spec.motor_body_length + 1) * (dot_num % 2),
+                (spec.motor_body_length + spec.gap_between_motor_layers) * layer_num,
             ),
         )
 
         # Create the motor shaft hole.
         p += bd.Cylinder(
             radius=0.25,
-            height=spec.motor_body_length * 2.5,
+            height=(
+                (
+                    spec.motor_body_length + spec.gap_between_motor_layers
+                    if layer_num == 0
+                    else 0
+                )
+                + (spec.motor_body_length + spec.gap_above_top_motor)
+            ),
             align=bde.align.ANCHOR_BOTTOM,
         ).translate(
             (
                 dot_x,
                 dot_y,
-                (spec.motor_body_length + 1) * (dot_num % 2),
+                (spec.motor_body_length + spec.gap_between_motor_layers) * layer_num,
             ),
+        )
+
+    # Show where the braille dots would be.
+    for cell_x, cell_y, dot_offset_x, dot_offset_y in product(
+        evenly_space_with_center(
+            count=spec.cell_count_x,
+            spacing=spec.inter_cell_pitch_x,
+        ),
+        evenly_space_with_center(
+            count=spec.cell_count_y,
+            spacing=spec.inter_cell_pitch_y,
+        ),
+        evenly_space_with_center(count=2, spacing=spec.dot_pitch_x),
+        evenly_space_with_center(count=3, spacing=spec.dot_pitch_y),
+    ):
+        dot_x = cell_x + dot_offset_x
+        dot_y = cell_y + dot_offset_y
+
+        # Create the braille dot.
+        p += bd.Cylinder(
+            radius=0.5,
+            height=0.5,
+            align=bde.align.ANCHOR_BOTTOM,
+        ).translate(
+            (
+                dot_x,
+                dot_y,
+                (
+                    (spec.motor_body_length * 2)
+                    + spec.gap_between_motor_layers
+                    + spec.gap_above_top_motor
+                ),
+            )
         )
 
     return p
